7a27eac1871b4f99fd5cd666ab9e4b02f66a4d31
suzhenjiang <suzhenjiang1031@gmail.com>
2024-08-05 21:13:12 +0800

    exercise2

---
+++ test/test/src/exercise2.java
@@ -0,0 +1,11 @@
+public class exercise2 {
+    public static void main(String[] args) {
+        int i;
+        for (i = 1; i <= 5; i++){
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
