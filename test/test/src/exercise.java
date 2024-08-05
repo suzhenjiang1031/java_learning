01660b6b7a4077aea9971e2ff8dad5fc582d4a80
suzhenjiang <suzhenjiang1031@gmail.com>
2024-08-05 21:12:47 +0800

    exercise1

---
+++ test/test/src/exercise.java
@@ -0,0 +1,35 @@
+public class exercise {
+    public static void method1() {
+        System.out.println("I am method 1.");
+    }
+
+    public static void method2() {
+        method1();
+        System.out.println("I am method 2.");
+    }
+
+    public static void method3(){
+        method2();
+        System.out.println("I am method 3.");
+        method1();
+    }
+
+    public static void main(String[] args){
+        method1();
+        method3();
+        method2();
+        method3();
+    }
+}
+/*I am method1
+I am method1
+I am method2
+I am method3
+I am method1
+I am method1
+I am method2
+I am method1
+I am method2
+I am method3
+I am method1
+ */
