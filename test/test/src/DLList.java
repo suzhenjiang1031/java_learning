e31d592fad4d2e73a67f3861e195e80b44703f1f
suzhenjiang <suzhenjiang1031@gmail.com>
2024-08-06 21:26:38 +0800

    DLList

---
+++ test/test/src/DLList.java
@@ -0,0 +1,69 @@
+//addLast getLast RemoveLast
+public class DLList {
+    private static class IntNode {
+        public IntNode prev;
+
+        public int item;
+        public  IntNode next;
+
+        public IntNode(int x, IntNode n, IntNode p) {
+            prev = p;
+            item = x;
+            next  = n;
+        }
+    }
+
+    private IntNode sentinel;
+    private int size;
+
+    public DLList() {
+        sentinel = new IntNode(63, null, null);
+        size = 0;
+    }
+
+    public DLList(int x) {
+        sentinel = new IntNode(63, sentinel.next, null);
+        sentinel.next = new IntNode(x, null, sentinel);
+        size = 1;
+    }
+
+    public void addlast(int x) {
+        size = size + 1;
+        IntNode p = sentinel;
+
+        while (p.next != null) {
+            p = p.next;
+        }
+        p.next = new IntNode(x, null, p);
+    }
+
+    public int getlast() {
+        IntNode p = sentinel;
+        while (p.next != null) {
+            p = p.next;
+        }
+        return p.item;
+    }
+
+    public void removelast() {
+        IntNode p = sentinel;
+        while (p.next != null) {
+            p = p.next;
+        }
+        p.next = null;
+    }
+
+    public int returnsize() {
+        return size;
+    }
+
+    public static void main(String[] args) {
+        DLList d = new DLList();
+        d.addlast(2);
+        d.addlast(3);
+        d.addlast(4);
+        d.addlast(5);
+        d.getlast();
+    }
+
+}
