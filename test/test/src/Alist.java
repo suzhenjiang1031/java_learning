642f149309863fbc384f0a0faf5739d77c1b177f
suzhenjiang <suzhenjiang1031@gmail.com>
2024-08-07 22:25:12 +0800

    AList

---
+++ test/test/src/Alist.java
@@ -0,0 +1,51 @@
+public class Alist<Item> {
+    private Item[] items;
+    private int size;
+
+    //Creates an empty list.
+    public Alist() {
+        items = (Item[]) new Object[100];
+        size = 0;
+    }
+
+    //Resizes the underlying array to the target capacity.
+    private void resize(int capacity) {
+        Item[] a = (Item[]) new Object[capacity];
+        System.arraycopy(items, 0, a, 0, size);
+        items = a;
+    }
+
+    //Inserts X into the back of the list
+    public void addLast(Item x) {
+        if (size == items.length) {
+            resize(size + 1);
+        }
+
+        items[size] = x;
+        size = size + 1;
+    }
+
+    //Returns the item from the back of the list.
+    public Item getLast() {
+        return items[size - 1];
+    }
+
+    //Get the ith item in the list (0 is the first).
+    public Item get(int i) {
+        return items[i];
+    }
+
+    //Return the number of items in the list.
+    public int size() {
+        return size;
+    }
+
+    //Deletes item from back of the list and returns deleted item.
+    public Item removeLast() {
+        Item x = getLast();
+        items[size - 1] = null;
+        size = size -1;
+        return x;
+    }
+
+}
--- test/test/src/DLList.java
+++ test/test/src/DLList.java
@@ -19,35 +19,44 @@ public class DLList {
 
     public DLList() {
         sentinel = new IntNode(63, null, null);
+        last = sentinel;
         size = 0;
     }
 
     public DLList(int x) {
-        sentinel = new IntNode(63, sentinel.next, null);
-        sentinel.next = new IntNode(x, null, sentinel);
+        sentinel = new IntNode(63, last, null);
+        last = new IntNode(x, null, sentinel);
+        sentinel.next = last;
         size = 1;
+    }
+
+    public void addfirst(int x) {
+        size = size + 1;
+        IntNode newNode = new IntNode(x, sentinel.next, sentinel);
+        if (sentinel.next != null) {
+            sentinel.next.prev = newNode;
+        }
+        sentinel.next = newNode;
+        if (size == 0) {
+            last = newNode;
+        }
     }
 
     public void addlast(int x) {
         size = size + 1;
-        last.next = new IntNode(x, sentinel, null);
-        last = last.next;
+        IntNode newNode = new IntNode(x, null, last);
+        last.next = newNode;
+        last = newNode;
     }
 
     public int getlast() {
-        IntNode p = sentinel;
-        while (p.next != null) {
-            p = p.next;
-        }
-        return p.item;
+        return last.item;
     }
 
     public void removelast() {
-        IntNode p = sentinel;
-        while (p.next != null) {
-            p = p.next;
-        }
-        p.next = null;
+        size = size - 1;
+        last = last.prev;
+        last.next = null;
     }
 
     public int returnsize() {
@@ -55,11 +64,13 @@ public class DLList {
     }
 
     public static void main(String[] args) {
-        DLList d = new DLList();
+        DLList d = new DLList(20);
         d.addlast(2);
         d.addlast(3);
         d.addlast(4);
         d.addlast(5);
+        d.removelast();
+        d.addfirst(1);
         int s = d.getlast();
         System.out.println(s);
     }
