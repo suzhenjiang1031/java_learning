d2bf5e796c5f8701069f0e0c3055a61d1d4a8633
suzhenjiang <suzhenjiang1031@gmail.com>
2024-08-17 22:50:24 +0800

    inheritance

---
+++ cs61b_homework/homework/src/inheritance2/RotatingSLList.java
@@ -0,0 +1,24 @@
+package inheritance2;
+
+public class RotatingSLList<Item> extends SLList<Item> {
+    //To do: Implement RotatingSLList such that code compiles and outputs
+    //correct result.
+    //Rotates list to the right.
+    public void rotateRight() {
+        Item x = removeLast();
+        addFirst(x);
+    }
+
+    public static void main(String[] args) {
+        RotatingSLList<Integer> rsl = new RotatingSLList<>();
+        //Creates SLList: [10, 11, 12, 13]
+        rsl.addLast(10);
+        rsl.addLast(11);
+        rsl.addLast(12);
+        rsl.addLast(13);
+
+        //should be: [13, 10, 11, 12]
+        rsl.rotateRight();
+        rsl.print();
+    }
+}
