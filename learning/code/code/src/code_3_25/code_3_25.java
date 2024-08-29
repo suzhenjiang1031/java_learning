17da3f80651a53c8d2df040ade24d7678505e4a4
suzhenjiang <suzhenjiang1031@gmail.com>
2024-08-29 13:36:19 +0800

    test

---
+++ learning/code/code/src/code_3_25/code_3_25.java
@@ -0,0 +1,22 @@
+package code_3_25;
+
+
+import java.util.Scanner;
+
+public class code_3_25 {
+    //流程控制语句
+    //顺序结构
+    //分支结构
+    //循环结构
+/*    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入女婿的酒量");
+        int wine = sc.nextInt();
+
+        if (wine > 2){
+            System.out.println("小伙子不错哟");
+        }
+    }*/
+
+}
+
