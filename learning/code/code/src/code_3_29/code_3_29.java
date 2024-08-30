3a323ae2d655280d25bfa755b95179ab8c2d6842
suzhenjiang <suzhenjiang1031@gmail.com>
2024-08-31 02:24:35 +0800

    test

---
+++ learning/code/code/src/code_3_29/code_3_29.java
@@ -0,0 +1,12 @@
+package code_3_29;
+
+public class code_3_29 {
+    public static void main(String[] args) {
+        int[] arr1 = new int[]{11,12,13,14};
+        int[] arr2 = {11,12,13,14};
+        String[] arr3 = new String[]{"zhangshan", "lisi", "wangwu"};
+        String[] arr4 = {"zhangshan", "lisi", "wangwu"};
+        double[] arr5 = new double[]{1.93, 1.75, 1.71, 1.81};
+        double[] arr6 = {1.93, 1.75, 1.71, 1.81};
+    }
+}
