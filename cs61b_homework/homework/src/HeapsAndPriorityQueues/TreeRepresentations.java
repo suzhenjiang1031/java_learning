f6624c9b9d8ae227edec55718f41f779baf48b13
suzhenjiang <suzhenjiang1031@gmail.com>
2024-09-08 15:07:17 +0800

    TreeRepresentations

---
+++ cs61b_homework/homework/src/HeapsAndPriorityQueues/TreeRepresentations.java
@@ -0,0 +1,30 @@
+package HeapsAndPriorityQueues;
+
+import com.sun.source.tree.Tree;
+
+import javax.swing.*;
+
+public class TreeRepresentations {
+    public class Tree1A<Key> {
+//        Key k;
+//        Tree1A left;
+//        Tree1A middle;
+//        Tree1A right;
+//
+//        Key k;
+//        Tree1B[] children;
+//        key K;
+//        Tree1C favoredChild;
+//        Tree1C sibling;
+//    }
+//        public void swim(int k) {
+//        if (Keys[parent(k)] > keys[k]) {
+//            swap(k, parent(k));
+//            swim(parent(k));
+//        }
+//        }
+//        public int parent(int k) {
+//            return (k - 1) / 2;
+        }
+    }
+}
