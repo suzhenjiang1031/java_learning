4915bdcdce8578bdce1ff10d8ce5ec45343a249e
suzhenjiang <suzhenjiang1031@gmail.com>
2024-09-15 14:23:27 +0800

    tries

---
+++ cs61b_homework/homework/src/PrefixOperationsandTries/tries.java
@@ -0,0 +1,22 @@
+package PrefixOperationsandTries;
+
+import javax.xml.crypto.Data;
+
+public class tries {
+    private static final int R = 128; //ASCII
+    private Node root; //Root of trie
+    private boolean isKey;
+    private DataIndexedCharMap next;
+    private Node(boolean b, int R) {
+        isKey = b;
+        next = new DataIndexedCharMap<Node>(R);
+    }
+
+    public class DataIndexedCharMap<V> {
+        private V[] items;
+        public DataIndexedCharMap(int R) {
+            items = (V[]) new Object[R];
+        }
+    }
+
+}
